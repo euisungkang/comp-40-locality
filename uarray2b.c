970fff7983431a4621a40144e968f6a08c599658
Easton Kang <euisungkang@gmail.com>
2019-09-30 21:27:46 -0400

    First commit

---
+++ uarray2b.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "uarray2b.h"
+
+extern T UArray2b_new (int width, int height, int size, int blocksize)
+{
+
+}
+/*
+extern T UArray2b_new_64K_block(int width, int height, int size);
+extern void UArray2b_free (T *array2b);
+extern int UArray2b_width (T array2b);
+extern int UArray2b_height (T array2b);
+extern int UArray2b_size (T array2b);
+extern int UArray2b_blocksize(T array2b);
+
+extern void *UArray2b_at(T array2b, int column, int row);
+extern void UArray2b_map(T array2b, void apply(int col, int row, T array2b, void *elem, void *cl), void *cl);*/
