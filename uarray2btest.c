1c34f733b901da7a4a8ce001ef54c0599715465d
Mico <theogenemicomyiza@gmail.com>
2019-10-03 12:12:11 -0400

    testing

--- uarray2btest.c
+++ uarray2btest.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "uarray2b.h"
+//#include "uarray2.h"
 
 int main()
 {
@@ -12,22 +13,19 @@ int main()
 			int *temp = UArray2b_at(array, i, j);
 			*temp = count++;
 
-
 			printf("temp is %d ", *temp);
 			printf("UArray2b_at is %d\n", *((int *)UArray2b_at(array, i, j)));
 		}
 	}	
 	
 
-	for (int i = 0; i < 4; ++i)
-	{
-		for (int j = 0; j < 4; ++j)
-		{
+	for (int i = 0; i < 4; ++i){
+		for (int j = 0; j < 4; ++j){
 			printf("%d  ", *((int *)UArray2b_at(array, i, j)));	
 		}
 		printf("\n");
 	}
-	
+
 	printf("Width: %d\n", UArray2b_width(array));
 	printf("Height: %d\n", UArray2b_height(array));
 	printf("Size: %d\n", UArray2b_size(array));
@@ -35,5 +33,28 @@ int main()
 
 
 	UArray2b_free(&array);
+
+
+
+
+	// //test UArray2
+	// UArray2_T  testing = UArray2_new(4,4, sizeof(int));
+
+	// int count = 0;
+	// for (int i = 0; i < 4; i++) {
+	// 	for (int j = 0; j < 4; j++) {
+	// 		int *temp = UArray2_at(testing, i, j);
+	// 		*temp = count++;
+	// 	}
+	// }
+
+	// //print elements
+	// for (int i = 0; i < 4; ++i){
+	// 	for (int j = 0; j < 4; ++j){
+	// 		printf("%d  ", *((int *)UArray2_at(testing, i, j)));	
+	// 	}
+	// 	printf("\n");
+	// }
+
 	return 0;
 }
