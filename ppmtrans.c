854b59e2280b09301859ea7fce2a71994d7299d8
Easton Kang <euisungkang@gmail.com>
2019-10-03 19:26:30 -0400

    small update to ppm

--- ppmtrans.c
+++ ppmtrans.c
@@ -18,7 +18,7 @@
                                 argv[0]);                       \
                 exit(1);                                        \
         }                                                       \
-} while (0)
+} while (0);
 
 static void
 usage(const char *progname)
@@ -28,6 +28,10 @@ usage(const char *progname)
                         progname);
         exit(1);
 }
+
+/* Function Prototypes */
+void rotate_0(A2Methods_T, A2Methods_mapfun);
+void rotate_90(A2Methods_T, A2Methods_mapfun);
 
 int main(int argc, char *argv[]) 
 {
@@ -43,16 +47,21 @@ int main(int argc, char *argv[])
         A2Methods_mapfun *map = methods->map_default; 
         assert(map);
 
+
         for (i = 1; i < argc; i++) {
+
                 if (strcmp(argv[i], "-row-major") == 0) {
                         SET_METHODS(uarray2_methods_plain, map_row_major, 
 				    "row-major");
+
                 } else if (strcmp(argv[i], "-col-major") == 0) {
                         SET_METHODS(uarray2_methods_plain, map_col_major, 
 				    "column-major");
+
                 } else if (strcmp(argv[i], "-block-major") == 0) {
                         SET_METHODS(uarray2_methods_blocked, map_block_major,
                                     "block-major");
+
                 } else if (strcmp(argv[i], "-rotate") == 0) {
                         if (!(i + 1 < argc)) {      /* no rotate value */
                                 usage(argv[0]);
@@ -68,14 +77,25 @@ int main(int argc, char *argv[])
                         if (!(*endptr == '\0')) {    /* Not a number */
                                 usage(argv[0]);
                         }
+
+                        if (rotation == 0) {
+                            rotate_0(methods, map);
+                        } else if (rotation == 90) {
+                            rotate_90(methods, map);
+                        }
+
+
                 } else if (strcmp(argv[i], "-time") == 0) {
                         time_file_name = argv[++i];
+
                 } else if (*argv[i] == '-') {
                         fprintf(stderr, "%s: unknown option '%s'\n", argv[0],
 				argv[i]);
+
                 } else if (argc - i > 1) {
                         fprintf(stderr, "Too many arguments\n");
                         usage(argv[0]);
+
                 } else {
                         break;
                 }
@@ -83,3 +103,17 @@ int main(int argc, char *argv[])
 
         assert(0);              // the rest of this function is not yet implemented
 }
+
+void rotate_0(A2Methods_T methods, A2Methods_mapfun *map)
+{
+    (void) methods;
+    return;
+}
+
+void rotate_90(A2Methods_T methods, A2Methods_mapfun *map)
+{
+    map()
+}
+
+
+#undef SET_METHODS
